222f67856405cb792b2714c8ec68880a3d635274
realImpaulator <paulstark@web.de>
2025-05-19 17:32:52 +0200

    add

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -2,6 +2,8 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(req) {
+  const body = await req.json(); // 👈 required to consume the request body
+
   return NextResponse.json({
     version: 'vNext',
     content: {
@@ -9,7 +11,7 @@ export async function POST(req) {
       frames: [
         {
           image: {
-            url: 'https://farcaster-car-game.vercel.app/screenshot.png', // Change this
+            url: 'https://farcaster-car-game.vercel.app/icon.png',
           },
           buttons: [
             {
